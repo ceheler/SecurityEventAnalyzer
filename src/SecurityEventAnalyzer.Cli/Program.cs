cc072d29f62121f59aa6a7af313fc18f32468821
ceheler <ceheler@gmail.com>
2026-08-26 16:34:28 -0700

    Added base functionality of reading input file

---
+++ src/SecurityEventAnalyzer.Cli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Provide user with help if no comand line argument is passed to the program
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Error: No command argument specified.");
+            Console.WriteLine("Usage: SecurityEventAnalyzer <path-to-log>");
+            return;
+            // TODO: Validate path, encompas in try catch for error handling
+        }
+            string inputFile = args[0];
+
+            if (System.IO.File.Exists(inputFile))
+            {
+                string content = System.IO.File.ReadAllText(inputFile);
+                Console.WriteLine("File provided");
+                Events[] events = JsonSerializer.Deserialize<Events[]>(content);
+                Console.WriteLine("Processed " + events.Length + " events");
+            }
+        }
+}
+
+public class Events
+{
+    public DateTime timestamp { get; set; }
+    public string EventID { get; set; }
+    public string Computer { get; set; }
+    public string Username { get; set; }
+    public string SourceIP { get; set; }
+    public string EventType { get; set; }
+    public string Level { get; set; }
+    public string Message { get; set; }
+}
